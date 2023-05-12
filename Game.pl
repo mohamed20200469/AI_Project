b97a206a4b8f7ecc51dd601c85c1bdada6e60be5
mohamed20200469 <117383066+mohamed20200469@users.noreply.github.com>
2023-05-12 20:14:52 +0200

    Alpha beta pruning
    Added alpha beta pruning instead of min-max.

--- Game.pl
+++ Game.pl
@@ -30,7 +30,7 @@ computerTurn([Computer,State], NextState),
 play(NextState, Human).
 
 computerTurn(State, Next):-
-minimax(State, Next, _).
+alphabeta(State, -100, 100, Next, _).
 
 isTerminal(State):-
 getWinner(State, Winner), write(Winner), write(' wins!'), nl, !.
@@ -47,7 +47,7 @@ getWinner(State, Winner):-
 (State = [_,_,Z,_,_,Z,_,_,Z], !);
 (State = [Z,_,_,_,Z,_,_,_,Z], !);
 (State = [_,_,Z,_,Z,_,Z,_,_], !) ), Z \= '-', Winner = Z.
-
+	 
 draw([]):-!.
 
 draw([H|T]):-
@@ -56,30 +56,44 @@ write(H),
 (0 is N mod 3 -> nl ; write(' ')),
 draw(T).
 
-minimax(Pos, BestNextPos, Val):-
+
+alphabeta(Pos, Alpha, Beta, BestNextPos, Val):-
 bagof(NextPos, move(Pos, NextPos), NextPosList),
-best(NextPosList, BestNextPos, Val), !.
-
-minimax(Pos, _, Val):-
+best(NextPosList, Alpha, Beta, BestNextPos, Val), !.
+alphabeta(Pos, _, _,_, Val):-
 utility(Pos, Val).
+best([Pos|_],Alpha, Beta, _, Val):-
+Beta =< Alpha, !,
+(isMinPlayer(Pos) -> Val is Alpha ; Val is Beta).
 
-best([Pos], Pos, Val):-
-minimax(Pos, _, Val), !.
-
-best([Pos1 | Tail], BestPos, BestVal) :-
-minimax(Pos1, _, Val1),
-best(Tail, Pos2, Val2),
+best([Pos], Alpha, Beta, Pos, Val):-
+alphabeta(Pos, Alpha, Beta, _, Val), !.
+best([Pos1 | Tail], Alpha, Beta, BestPos, BestVal) :-
+alphabeta(Pos1, Alpha, Beta, _, Val1),
+updateValues(Pos1, Val1, Alpha, Beta, NewAlpha, NewBeta),
+best(Tail, NewAlpha, NewBeta, Pos2, Val2),
 betterOf(Pos1, Val1, Pos2, Val2, BestPos, BestVal).
+updateValues(Pos1, Value, Alpha, Beta, NewAlpha, Beta):-
+isMinPlayer(Pos1), !,
+(Value > Alpha -> (NewAlpha is Value, !)
+; NewAlpha is Alpha
 
-betterOf(Pos1, Val1, Pos2, Val2, BestPos, BestVal) :-
-isMinPlayer(Pos1), % next (Pos1) is MIN, so the parent is MAX
-(Val1 >= Val2 -> (BestPos = Pos1, BestVal is Val1, !); 
-(BestPos = Pos2, BestVal is Val2)), !.
+).
+updateValues(_, Value, Alpha, Beta, Alpha, NewBeta):-
+(Value < Beta -> (NewBeta is Value, !)
+; NewBeta is Beta
 
+).
 betterOf(Pos1, Val1, Pos2, Val2, BestPos, BestVal) :-
-(
-Val1 =< Val2 -> (BestPos = Pos1, BestVal is Val1, !); 
-(BestPos = Pos2, BestVal is Val2)
+isMinPlayer(Pos1),
+(Val1 >= Val2 -> (BestPos = Pos1, BestVal is Val1, !)
+; (BestPos = Pos2, BestVal is Val2)
+
+), !.
+betterOf(Pos1, Val1, Pos2, Val2, BestPos, BestVal) :-
+(Val1 =< Val2 -> (BestPos = Pos1, BestVal is Val1, !)
+; (BestPos = Pos2, BestVal is Val2)
+
 ), !.
 
 move([Player, State], Next):-
